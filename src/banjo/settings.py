8703a35568ea61e9d608a76c8c252c2e8291cbfc
Chris Proctor <chris@chrisproctor.net>
2021-10-24 15:51:18 -0400

    removing middleware causing trouble

--- src/banjo/settings.py
+++ src/banjo/settings.py
@@ -18,13 +18,6 @@ INSTALLED_APPS = [
     "app",
 ]
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 SHELL_PLUS_DONT_LOAD = [
     "banjo",
