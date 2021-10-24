772f13a61bb39127b47702e0bce1dbafce8d657e
Chris Proctor <chris@chrisproctor.net>
2021-10-24 15:43:57 -0400

    Adding middleware expected by heroku

--- src/banjo/settings.py
+++ src/banjo/settings.py
@@ -17,6 +17,15 @@ INSTALLED_APPS = [
     "banjo",
     "app",
 ]
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 SHELL_PLUS_DONT_LOAD = [
     "banjo",
 ]
