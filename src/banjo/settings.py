4a502059fa48908d00945e3951cebcdf9a5ccb5e
Chris Proctor <chris@chrisproctor.net>
2021-11-04 13:39:49 -0400

    Adding lots of settings which turn out to be necessary for template rendering

--- src/banjo/settings.py
+++ src/banjo/settings.py
@@ -10,13 +10,48 @@ DATABASES = {
     }
 }
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 ALLOWED_HOSTS = ["*"]
 SECRET_KEY = "xxx"
 INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     "django_extensions",
     "banjo",
     "app",
 ]
 MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+STATIC_URL = '/static/'
+SHELL_PLUS_DONT_LOAD = [
+    "admin",
+    "auth",
+    "contenttypes",
+    "sessions",
 ]
 SHELL_PLUS_DJANGO_IMPORTS = False
